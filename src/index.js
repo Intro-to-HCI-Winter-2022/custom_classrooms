bf706f774cea99b8b40639c590d5ef74236159ca
jts307 <f003frq@dartmouth.edu>
2021-04-15 18:55:23 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+// counter for seconds passed
+let num = 0;
+setInterval(() => {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+}, 1000);
