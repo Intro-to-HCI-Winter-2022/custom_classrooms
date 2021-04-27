ca08869cc05e75b57f71689af9e5fdb0f23e6b52
jts307 <f003frq@dartmouth.edu>
2021-04-27 17:37:47 -0400

    finished sa4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-// es6 import
-import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
 
-// React Test
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('main'));
-
-// counter for seconds passed
-let num = 0;
-setInterval(() => {
-  num += 1;
-  $('#main').html(`You've been on this page for ${num} seconds.`);
-}, 1000);
--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,12 @@ const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { 
 
 module.exports = {
   mode: env,
+  output: { publicPath: '/' },
   entry: ['./src'], // this is where our app lives
   devtool: 'source-map', // this enables debugging with source in chrome devtools
   devServer: {
     hot: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
